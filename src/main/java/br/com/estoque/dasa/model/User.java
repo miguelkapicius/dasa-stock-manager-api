1931d25de6d3fee07f11bb39cd893bae9892cb01
Miguel Kapicius <miguelkapicius@gmail.com>
2025-07-02 20:05:33 -0300

    first commit

---
+++ src/main/java/br/com/estoque/dasa/model/User.java
@@ -0,0 +1,13 @@
+package br.com.estoque.dasa.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+}
---
+++ src/test/java/br/com/estoque/dasa/DasaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.estoque.dasa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DasaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
